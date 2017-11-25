772cb1987f4c2251f5c6ebf6b6d9c4d69c7cd0cd
Minghao23 <hu_minghao@users.noreply.github.com>
2017-11-25 00:28:14 -0800

    Use db to record latencies

--- src/main/java/CS6650/as2/client/Main.java
+++ src/main/java/CS6650/as2/client/Main.java
@@ -5,7 +5,6 @@ import CS6650.as2.model.HttpAnalysis;
 import CS6650.as2.model.MyVert;
 import CS6650.as2.model.Record;
 import CS6650.as2.util.Stat;
-import org.glassfish.jersey.client.ClientProperties;
 
 import javax.ws.rs.ProcessingException;
 import javax.ws.rs.client.Client;
@@ -30,8 +29,8 @@ public class Main {
     static final private String host2 = "54.149.109.52";
     static final private String host3 = "54.202.247.75";
     static final private String singlehost = "54.245.182.24";
-    //static final private String host = "myELB-979566651.us-west-2.elb.amazonaws.com";
-    static final private String host = "localhost";
+    static final private String host = "myELB-979566651.us-west-2.elb.amazonaws.com";
+//    static final private String host = "localhost";
     static final private int port = 8080;
 
     static final String DAY1URL = "/Users/hu_minghao/CS6650/Assignment2/SkiResort-Client/files/BSDSAssignment2Day1.csv";
@@ -82,11 +81,11 @@ public class Main {
         String api = "/SkiResort-Server_war/rest/hello/load";
         long startTime = System.currentTimeMillis();
         Client client = ClientBuilder.newClient();
-        client.property(ClientProperties.CONNECT_TIMEOUT, 9999999);
-        client.property(ClientProperties.READ_TIMEOUT,    9999999);
+//        client.property(ClientProperties.CONNECT_TIMEOUT, 9999999);
+//        client.property(ClientProperties.READ_TIMEOUT,    9999999);
         ArrayList<PostRecord> postTasks = new ArrayList<PostRecord>();
         Stat stat = new Stat();
-        for (int i = 0; i < 20000; i++) { // test in 10000 data
+        for (int i = 0; i < records.size(); i++) { // test in 10000 data
             postTasks.add(new PostRecord(protocol, host, port, api, records.get(i), client, stat));
         }
         ExecutorService pool = Executors.newFixedThreadPool(taskSize);
@@ -112,7 +111,6 @@ public class Main {
         System.out.println("> Median latency: " + stat.getMedianLatency());
         System.out.println("> 95th percentile latency: " + stat.get95thLatency());
         System.out.println("> 99th percentile latency: " + stat.get99thLatency());
-
     }
 
     public void getTasks(int dayNum) {
@@ -123,7 +121,7 @@ public class Main {
         ArrayList<GetMyVert> getMyVerts = new ArrayList<GetMyVert>();
         Stat stat = new Stat();
         for (int i = 1; i <= 3000; i++) {
-            getMyVerts.add(new GetMyVert(protocol, singlehost, port, i, dayNum, client, stat));
+            getMyVerts.add(new GetMyVert(protocol, host, port, i, dayNum, client, stat));
         }
         ExecutorService pool = Executors.newFixedThreadPool(100);// fix in 100 threads
         try {
@@ -150,13 +148,7 @@ public class Main {
         System.out.println("> 99th percentile latency: " + stat.get99thLatency());
     }
 
-    public void getHttpAnalysis(String hostNum) {
-        String host = null;
-        switch(hostNum) {
-            case "host1" : host = host1;break;
-            case "host2" : host = host2;break;
-            case "host3" : host = host3;break;
-        }
+    public void getHttpAnalysis() {
         Client client = ClientBuilder.newClient();
         URL url = null;
         String api = "/SkiResort-Server_war/rest/analysis/http";
@@ -167,32 +159,51 @@ public class Main {
         }
         WebTarget webTarget = client.target(url.toString());
         Response response;
-        HttpAnalysis result = null;
+        HttpAnalysis result;
         try {
             response = webTarget.request().get();
             result = response.readEntity(HttpAnalysis.class);
             response.close();
             client.close();
             System.out.println(">>>>>> Http Requests Analysis <<<<<<");
-            System.out.println("> Target host: " + host);
-            System.out.println("> Number of requests received: " + result.getTotalRequestNum());
-            System.out.println("> Number of failed requests: " + result.getFailedRequestNum());
-            System.out.println("> Mean latency: " + result.getMeanLatency());
-            System.out.println("> Median latency: " + result.getMedianLatency());
-            System.out.println("> 95th percentile latency: " + result.getThe95thLatency());
-            System.out.println("> 99th percentile latency: " + result.getThe99thLatency());
+            System.out.println("");
+            System.out.println("---- All three servers ----");
+            System.out.println("> Number of successful requests: " + result.getAll_RequestNum());
+            System.out.println("> Number of failed requests: " + result.getAll_failedRequestNum());
+            System.out.println("> Mean latency: " + result.getAll_meanLatency());
+            System.out.println("> Median latency: " + result.getAll_medianLatency());
+            System.out.println("> 95th latency: " + result.getAll_the95thLatency());
+            System.out.println("> 99th latency: " + result.getAll_the99thLatency());
+            System.out.println("");
+            System.out.println("---- Server 1 ----");
+            System.out.println("> Number of successful requests: " + result.getH1_RequestNum());
+            System.out.println("> Number of failed requests: " + result.getH1_failedRequestNum());
+            System.out.println("> Mean latency: " + result.getH1_meanLatency());
+            System.out.println("> Median latency: " + result.getH1_medianLatency());
+            System.out.println("> 95th latency: " + result.getH1_the95thLatency());
+            System.out.println("> 99th latency: " + result.getH1_the99thLatency());
+            System.out.println("");
+            System.out.println("---- Server 2 ----");
+            System.out.println("> Number of successful requests: " + result.getH2_RequestNum());
+            System.out.println("> Number of failed requests: " + result.getH2_failedRequestNum());
+            System.out.println("> Mean latency: " + result.getH2_meanLatency());
+            System.out.println("> Median latency: " + result.getH2_medianLatency());
+            System.out.println("> 95th latency: " + result.getH2_the95thLatency());
+            System.out.println("> 99th latency: " + result.getH2_the99thLatency());
+            System.out.println("");
+            System.out.println("---- Server 3 ----");
+            System.out.println("> Number of successful requests: " + result.getH3_RequestNum());
+            System.out.println("> Number of failed requests: " + result.getH3_failedRequestNum());
+            System.out.println("> Mean latency: " + result.getH3_meanLatency());
+            System.out.println("> Median latency: " + result.getH3_medianLatency());
+            System.out.println("> 95th latency: " + result.getH3_the95thLatency());
+            System.out.println("> 99th latency: " + result.getH3_the99thLatency());
         } catch (ProcessingException e) {
             e.printStackTrace();
         }
     }
 
-    public void getDBAnalysis(String hostNum) {
-        String host = null;
-        switch(hostNum) {
-            case "host1" : host = host1;break;
-            case "host2" : host = host2;break;
-            case "host3" : host = host3;break;
-        }
+    public void getDBAnalysis() {
         Client client = ClientBuilder.newClient();
         URL url = null;
         String api = "/SkiResort-Server_war/rest/analysis/db";
@@ -203,23 +214,79 @@ public class Main {
         }
         WebTarget webTarget = client.target(url.toString());
         Response response;
-        DBAnalysis result = null;
+        DBAnalysis result;
         try {
             response = webTarget.request().get();
             result = response.readEntity(DBAnalysis.class);
             response.close();
             client.close();
             System.out.println(">>>>>> Database Queries Analysis <<<<<<");
-            System.out.println("> Target host: " + host);
-            System.out.println("> Number of queries sent: " + result.getTotalRequestNum());
-            System.out.println("> Mean latency: " + result.getMeanLatency());
-            System.out.println("> Median latency: " + result.getMedianLatency());
-            System.out.println("> 95th percentile latency: " + result.getThe95thLatency());
-            System.out.println("> 99th percentile latency: " + result.getThe99thLatency());
+            System.out.println("");
+            System.out.println("---- All three servers ----");
+            System.out.println("> Number of queries: " + result.getAll_QueryNum());
+            System.out.println("> Mean latency: " + result.getAll_meanLatency());
+            System.out.println("> Median latency: " + result.getAll_medianLatency());
+            System.out.println("> 95th latency: " + result.getAll_the95thLatency());
+            System.out.println("> 99th latency: " + result.getAll_the99thLatency());
+            System.out.println("");
+            System.out.println("---- Server 1 ----");
+            System.out.println("> Number of queries:: " + result.getH1_QueryNum());
+            System.out.println("> Mean latency: " + result.getH1_meanLatency());
+            System.out.println("> Median latency: " + result.getH1_medianLatency());
+            System.out.println("> 95th latency: " + result.getH1_the95thLatency());
+            System.out.println("> 99th latency: " + result.getH1_the99thLatency());
+            System.out.println("");
+            System.out.println("---- Server 2 ----");
+            System.out.println("> Number of queries: " + result.getH2_QueryNum());
+            System.out.println("> Mean latency: " + result.getH2_meanLatency());
+            System.out.println("> Median latency: " + result.getH2_medianLatency());
+            System.out.println("> 95th latency: " + result.getH2_the95thLatency());
+            System.out.println("> 99th latency: " + result.getH2_the99thLatency());
+            System.out.println("");
+            System.out.println("---- Server 3 ----");
+            System.out.println("> Number of queries: " + result.getH3_QueryNum());
+            System.out.println("> Mean latency: " + result.getH3_meanLatency());
+            System.out.println("> Median latency: " + result.getH3_medianLatency());
+            System.out.println("> 95th latency: " + result.getH3_the95thLatency());
+            System.out.println("> 99th latency: " + result.getH3_the99thLatency());
         } catch (ProcessingException e) {
             e.printStackTrace();
         }
     }
+//    public void getDBAnalysis(String hostNum) {
+//        String host = null;
+//        switch(hostNum) {
+//            case "host1" : host = host1;break;
+//            case "host2" : host = host2;break;
+//            case "host3" : host = host3;break;
+//        }
+//        Client client = ClientBuilder.newClient();
+//        URL url = null;
+//        String api = "/SkiResort-Server_war/rest/analysis/db";
+//        try{
+//            url = new URL(protocol, host, port, api);
+//        } catch (MalformedURLException e){
+//            e.printStackTrace();
+//        }
+//        WebTarget webTarget = client.target(url.toString());
+//        Response response;
+//        DBAnalysis result = null;
+//        try {
+//            response = webTarget.request().get();
+//            result = response.readEntity(DBAnalysis.class);
+//            response.close();
+//            client.close();
+//            System.out.println(">>>>>> Database Queries Analysis <<<<<<");
+//            System.out.println("> Target host: " + host);
+//            System.out.println("> Number of queries sent: " + result.getTotalRequestNum());
+//            System.out.println("> Mean latency: " + result.getMeanLatency());
+//            System.out.println("> Median latency: " + result.getMedianLatency());
+//            System.out.println("> 95th percentile latency: " + result.getThe95thLatency());
+//            System.out.println("> 99th percentile latency: " + result.getThe99thLatency());
+//        } catch (ProcessingException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     public void clearAnalysis(String hostNum) {
         String host = null;
@@ -298,15 +365,11 @@ public class Main {
         }
 
         if(args[0].equals("getHttpAnalysis")) {
-            main.getHttpAnalysis(args[1]);
+            main.getHttpAnalysis();
         }
 
         if(args[0].equals("getDBAnalysis")) {
-            main.getDBAnalysis(args[1]);
-        }
-
-        if(args[0].equals("clear")) {
-            main.clearAnalysis(args[1]);
+            main.getDBAnalysis();
         }
     }
 }
